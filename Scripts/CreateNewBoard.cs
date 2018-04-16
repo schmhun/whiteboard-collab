bce0fe2ba094e38af3fed7ca838ebc44eea007b8
Hunter Schmitt <schmhun@users.noreply.github.com>
2018-04-16 12:34:22 -0500

    Draw and Erase functionality
    Draw & Erase added, bug with frame rate drop on draw.

---
+++ Scripts/CreateNewBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNewBoard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public Rigidbody board;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody clone;
+            clone = Instantiate(board, transform.position, transform.rotation) as Rigidbody;
+        }
+    }
+
+    void OnPress()
+    {
+
+    }
+}

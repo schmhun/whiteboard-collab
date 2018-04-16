4cb6b0f4c85e2c3a8d2acbdff5a1b13a95945ccd
schmhun <schmhun@gmail.com>
2018-04-16 12:28:17 -0500

    Draw and Erase
    Functionality added, debugging a frame rate drop and stacking of drawn disks.

---
+++ Assets/Scripts/EraseOnBoard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EraseOnBoard : MonoBehaviour {
+    private GameObject thumbLeft;
+    private GameObject thumbRight;
+    private GameObject thumbPrefer;
+    private GameObject[] tips;
+    public bool preferRight = true;
+
+    // Use this for initialization
+    void Start () {
+        FindThumbs();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FindThumbs();
+	}
+
+    private void FindThumbs()
+    {
+        tips = GameObject.FindGameObjectsWithTag("Erase");
+        foreach (GameObject tip in tips)
+        {
+            if (tip.name.Contains("R"))
+            {
+                thumbRight = tip;
+                //Debug.Log("Have a right thumb.");
+            }
+            if (tip.name.Contains("L"))
+            {
+                thumbLeft = tip;
+                //Debug.Log("Have a left tip.");
+            }
+        }
+        if (preferRight)
+        {
+            thumbPrefer = thumbRight;
+        }
+        else
+        {
+            thumbPrefer = thumbLeft;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == thumbPrefer.name)
+        {
+            Debug.Log("Erasing things!");
+            Destroy(this.gameObject);
+        }
+    }
+}
